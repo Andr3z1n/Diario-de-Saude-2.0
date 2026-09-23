d3ee4fa7f0f58f857c4a14218fd55ea1a0c27aa2
Andre Ryan <andre.soares12@aluno.ce.gov.br>
2026-09-23 16:11:00 +0000

    ok

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ class RegistroAlimentar {
     //  quando criamos um novo objeto da classe. 
     // É nele que inicializamos os atributos do objeto.⬇
     constructor(cafeDamanha, almoco, jantar, lanches, caloriasEstimadas, Aguaml) {
+
+        //Criamos a data para sumir o "undefined" do título e do aviso
+        this.dataRegistro = new Date().toLocaleDateString('pt-BR')
+
         this.cafeDamanha = cafeDamanha
         this.almoco = almoco
         this.jantar = jantar
         this.lanches = caloriasEstimadas
         this.Aguaml = Aguaml
     }
-}
 
 // Aqui vamos usar um Actions (Método) Além de
 //  armazenar informações, um objeto também pode executar ações.
@@ -23,8 +26,9 @@ gerarResumo() {
 --------------------------------------------------
 🔥 Total de Calorias: ${this.caloriasEstimadas} kcal
 💧 Água Consumida:    ${this.aguaMl} ml
-==================================================
-`}
+==================================================`
+    }
+}
 
 
 class DiarioAlimentar {
@@ -38,25 +42,24 @@ class DiarioAlimentar {
         console.log(`✅ Dia ${registro.dataRegistro} adicionado com sucesso!`)
     }
 
-    exibirHistorico() {
+    exibirHistorico() { // ⬅ Verifica se a lista de dias salvos está totalmente vazia
         if (this.historico.length === 0) {
-            console.log("O diário ainda está vazio.")
-            return
+            console.log("O diário ainda está vazio.") // Avisa o usuário no console
+            return //Interrompe a função aqui mesmo para não executar o código abaixo
         }
         // Percorre a lista chamando o método de cada objeto armazenado
-        this.historico.forEach(registro => {
-            console.log(registro.gerarResumo())
+        this.historico.forEach(registro => { //⬅ Percorre a lista ('forEach' = para cada)
+            // pegando um "registro" por vez
+            console.log(registro.gerarResumo()) //Executa o método de formatar o texto e imprime o resultado no console
         })
     }
 }
 
-// --- Executando o teste na prática ---
+//Executando o teste
+const meuDiario = new DiarioAlimentar()  //⬅Instanciamos o gerenciador do diário
 
-// 1. Instanciamos o gerenciador do diário
-const meuDiario = new DiarioAlimentar();
-
-// 2. Criamos um novo registro (o operador 'new' dispara o 'constructor')
-const dia1 = new RegistroAlimentar(
+// 2. Criamos um novo registro (o operador "new" dispara o "constructor)
+const dia1 = new RegistroAlimentar( 
     "Tapioca com queijo coalho e café com leite",
     "Arroz, feijão corda, carne de sol acebolada e salada",
     "Cuscuz com ovos mexidos",
@@ -64,9 +67,10 @@ const dia1 = new RegistroAlimentar(
     1800,
     2200
 )
+//⬆ ocorre criar um registro alimentar do seu primeiro dia de dieta,
+//  usando as comidas reais que você consumiu e definindo as metas para
+//  as calorias e para a água.
 
-// 3. Salvamos o objeto dentro do diário
-meuDiario.adicionarDia(dia1)
+meuDiario.adicionarDia(dia1) //⬅ guardar o dia que você acabou de criar dentro do histórico do seu diário.
 
-// 4. Imprimimos o relatório no console
-meuDiario.exibirHistorico()
+meuDiario.exibirHistorico() // ⬅Exibe na tela do console os relatórios formatados de todos os dias guardados no diário
